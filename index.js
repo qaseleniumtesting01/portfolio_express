ebd85168d81077a13bb2cf7731764e06fb7eb0f8
gbi <gabriel.ladzaretti@gmail.com>
2021-10-30 23:17:54 +0300

    added redis db and visitors counting functionalitiy

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const path = require("path");
 const express = require("express");
+const redis = require("redis");
+const REDIS_PORT = process.env.PORT || 6397;
+const REDIS_URL = process.env.REDISGO_URL;
+const REDIS_SET_NAME = "userip";
+const REDIS_CNT = "counter";
+const routerWrapper = require("./routes/api/router");
+// Global consts
+const HOUR_S = 3600;
+const PORT = process.env.PORT || 5000;
+
+// Init app
 const app = express();
+// when server is behind a proxy
+// app.enable("trust proxy");
+
+// Init Redis
+const client = REDIS_URL ? redis.createClient(REDIS_URL) : redis.createClient();
+
+// Check for existance of a redis set
+client.exists(REDIS_SET_NAME, (err, found) => {
+  if (err) throw err;
+  if (!found)
+    client.sadd(REDIS_SET_NAME, "0.0.0.0", (err, data) =>
+      console.log("REDIS set created")
+    );
+});
+
+// Check for existance of a redis counter
+client.exists(REDIS_CNT, (err, found) => {
+  if (err) throw err;
+  if (!found)
+    client.set(REDIS_CNT, 0, (err, data) =>
+      console.log("REDIS counter created")
+    );
+});
+
+client.on("connect", () => {
+  console.log("redis: connected successfully");
+});
+
+client.on("error", (err) => {
+  console.log("Error " + err);
+});
 
 // Set router
-app.use("/", require("./routes/api/router"));
+app.use("/", routerWrapper(client, REDIS_SET_NAME, REDIS_CNT));
 
 // Set Static folder
 app.use(express.static(path.join(__dirname, "public")));
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- routes/api/router.js
+++ routes/api/router.js
@@ -1,9 +1,20 @@
 const path = require("path");
 const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-  console.log("served index html");
-  res.sendFile(path.join(__dirname, "/../../", "public", "index.html"));
-});
 
-module.exports = router;
+function wrapper(redis, set, cnt) {
+  const router = express.Router();
+  router.get("/", (req, res) => {
+    const userIp = req.ip === "::1" ? "127.0.0.1" : req.ip;
+    redis.sismember(set, userIp, (err, found) => {
+      if (!found) {
+        redis.sadd(set, userIp);
+        redis.incr(cnt);
+      }
+    });
+
+    res.sendFile(path.join(__dirname, "/../../", "public", "index.html"));
+  });
+  return router;
+}
+
+module.exports = wrapper;
